3ad7320d3e0f4dbed7bb8e99be6e40ef6a7d87ef
nantharupan <nantha86@gmail.com>
2013-09-10 07:52:34 +0200

    Resgisteruser Method added to the Registration Manager,&& Validate Email Method signature added

--- Lab project/src/RegistrationManager.java
+++ Lab project/src/RegistrationManager.java
@@ -1,4 +1,33 @@
 
 public class RegistrationManager {
 
+	
+	public boolean registerUser(String email,String password,String firstname, String surname)
+	{
+		boolean registered=false;
+		if(email != null && !email.isEmpty()|| password != null && !password.isEmpty()|| firstname != null && !firstname.isEmpty()||surname != null && !surname.isEmpty())
+		{
+			return registered;
+		}
+		else
+		{
+		if(this.validateEmail(email))
+		{
+		Account account= new Account(email,password,firstname,surname);
+		//TODO there should be a method in Account to add the user to the Data base.
+		
+		//TODO call that method to create the user in th date base
+		
+		return registered;	
+		}else
+		{
+			return registered;
+		}
+		}
+	}
+	//TODO validate weather email exist in the data base , need to have one method some where to check database 
+	private boolean validateEmail(String email)
+	{
+		return false;
+	}
 }
