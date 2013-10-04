1e211c1c4413de0928c5fd3eb4bd00616788eb28
nantharupan <nantha86@gmail.com>
2013-10-04 20:29:01 +0200

    Inventory service added

---
+++ InventoryService/src/imse/soc/grp4/InvalidProductIdFault.java
@@ -0,0 +1,8 @@
+package imse.soc.grp4;
+
+public class InvalidProductIdFault extends Exception {
+	
+	public InvalidProductIdFault(String message) {
+        super(message);
+    }
+}
---
+++ InventoryService/src/imse/soc/grp4/InventoryService.java
@@ -0,0 +1,38 @@
+package imse.soc.grp4;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
+import javax.jws.WebService;
+import javax.jws.soap.SOAPBinding;
+
+@WebService(targetNamespace = "http://imse.soc.grp4.org/inventoryservice")
+@SOAPBinding(style = SOAPBinding.Style.DOCUMENT, use = SOAPBinding.Use.LITERAL, parameterStyle = SOAPBinding.ParameterStyle.WRAPPED)
+public class InventoryService {
+
+	public String getProductAvailability(int productId)
+			throws InvalidProductIdFault {
+		if (productId < 1 || productId > 1000) {
+			throw new InvalidProductIdFault("Product Not Exist");
+		} else {
+			Integer ProductId = productId;
+
+			DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd");
+			String productavailability;
+
+			if (productId % 2 == 0) {
+				productavailability = ProductId.toString();
+			} else {
+				Calendar cal = Calendar.getInstance();
+
+				cal.add(Calendar.DATE, productId);
+
+				productavailability = dateFormat.format(cal.getTime());
+
+			}
+
+			return productavailability;
+		}
+	}
+}
