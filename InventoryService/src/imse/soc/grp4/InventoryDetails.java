5c79f40323eb99c8fb6ac289035215847dabae66
mlushpenko <iviakciivi@gmail.com>
2013-11-02 18:29:17 +0100

    Improved inventory service.

--- InventoryService/src/imse/soc/grp4/InventoryDetails.java
+++ InventoryService/src/imse/soc/grp4/InventoryDetails.java
@@ -10,63 +10,73 @@ import java.util.Date;
 public class InventoryDetails {
 
 	private final static String JDBC_URL = "jdbc:postgresql://129.69.214.67:5432/webshop";
-//	private final static String JDBC_URL = "jdbc:postgresql://localhost:5432/postgres";
+	// private final static String JDBC_URL =
+	// "jdbc:postgresql://localhost:5432/postgres";
 	private final static String JDBC_USERNAME = "postgres";
 	private final static String JDBC_PASSWORD = "max123";
+	private Connection conn = null;
+	private ResultSet rset = null;
 
-	public String getInventoryDetails(int productId) throws InvalidProductIdFault {
+	public InventoryDetails() {
+		// empty constructor should be used for getInventoryService() method
+		// because we expect result set to be product with specific ID
+		// what is done in getInventoryService() method
+	}
+
+	public InventoryDetails(String query) {
+		connectToDb(query);
+	}
+
+	private void connectToDb(String query) {
+		try {
+			Class.forName("org.postgresql.Driver");
+
+			conn = DriverManager.getConnection(JDBC_URL, JDBC_USERNAME,
+					JDBC_PASSWORD);
+
+			Statement stmt = conn.createStatement();
+
+			rset = stmt.executeQuery(query);
+		} catch (ClassNotFoundException e1) {
+			System.out.println("Exception while connection the driver");
+			e1.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public String getInventoryDetails(int productId)
+			throws InvalidProductIdFault {
 
 		String available = "";
- 
-	    try {
-				Class.forName("org.postgresql.Driver");
-			} catch (ClassNotFoundException e1) {
-				// TODO Auto-generated catch block
-				
-				System.out.println("Exception while connection the driver");
-				
-				e1.printStackTrace();
-			}		
-		Connection conn = null;
+		if (conn == null && rset == null) {
+			connectToDb("select * from product_db where id=" + productId);
+		}
+
 		try {
-			conn = DriverManager.getConnection(JDBC_URL, JDBC_USERNAME,JDBC_PASSWORD);
 
-			Statement stmt = null;
-
-			stmt = conn.createStatement();
-
-			String strSelect = "select * from product_db where id=" + productId;
-
-			ResultSet rset = null;
-
-			rset = stmt.executeQuery(strSelect);
-
-			if (!rset.isBeforeFirst() ) {    
+			if (!rset.isBeforeFirst()) {
 				throw new InvalidProductIdFault("Product Not Exist");
-				} else
-				{
-			
-			while (rset.next()) {
+			} else {
 
-				int availableitems = rset.getInt("stockamount");
-				Date availabledate = rset.getDate("availabilitydate");
-				int threshold = rset.getInt("stockthreshold");
+				while (rset.next()) {
 
-				if (availableitems > threshold) {
-					available = String.valueOf(availableitems);
-				} else {
-					available = String.valueOf(availabledate);
+					int availableitems = rset.getInt("stockamount");
+					Date availabledate = rset.getDate("availabilitydate");
+					int threshold = rset.getInt("stockthreshold");
+
+					if (availableitems > threshold) {
+						available = String.valueOf(availableitems);
+					} else {
+						available = String.valueOf(availabledate);
+					}
+
 				}
-
 			}
-				}
 		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-		//	System.out.println(e.toString());
-			
-			
-			available=e.toString();
-			
+
+			available = e.toString();
+
 			e.printStackTrace();
 		}
 
--- InventoryService/src/imse/soc/grp4/InventoryService.java
+++ InventoryService/src/imse/soc/grp4/InventoryService.java
@@ -10,30 +10,41 @@ import javax.jws.soap.SOAPBinding;
 @WebService(targetNamespace = "http://imse.soc.grp4.org/inventoryservice")
 @SOAPBinding(style = SOAPBinding.Style.DOCUMENT, use = SOAPBinding.Use.LITERAL, parameterStyle = SOAPBinding.ParameterStyle.WRAPPED)
 public class InventoryService {
+	private final static int SPECIAL_CODE = 123456789;
+	private final static int RESUPPLY_AMOUNT = 50;
 
 	public String getProductAvailability(int productId)
 			throws InvalidProductIdFault {
-		if (productId < 1 || productId > 1000) {
+		if (productId > SPECIAL_CODE) {
+			int realID = productId - SPECIAL_CODE;
+			InventoryDetails inventory = new InventoryDetails("UPDATE product_db SET stockamount = stockamount +" + RESUPPLY_AMOUNT +" WHERE id =" + realID);
+			//reset inventory object
+			inventory = new InventoryDetails();
+			String stock = inventory.getInventoryDetails(realID);
+			return stock;
+		} else if (productId < 1 || productId > 1000) {
 			throw new InvalidProductIdFault("Product Not Exist");
 		} else {
 			Integer ProductId = productId;
 
 			DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd");
-			String productavailability ="none";
+			String productavailability = "none";
 			System.out.println(productavailability);
-/*			if (productId % 2 == 0) {
-				productavailability = ProductId.toString();
-			} else {
-				Calendar cal = Calendar.getInstance();
+			/*
+			 * if (productId % 2 == 0) { productavailability =
+			 * ProductId.toString(); } else { Calendar cal =
+			 * Calendar.getInstance();
+			 * 
+			 * cal.add(Calendar.DATE, productId);
+			 * 
+			 * productavailability = dateFormat.format(cal.getTime());
+			 * 
+			 * }
+			 */
 
-				cal.add(Calendar.DATE, productId);
-
-				productavailability = dateFormat.format(cal.getTime());
-
-			}*/
-			
 			InventoryDetails inventoryDetails = new InventoryDetails();
-			productavailability =	inventoryDetails.getInventoryDetails(productId);
+			productavailability = inventoryDetails
+					.getInventoryDetails(productId);
 			System.out.println(productavailability);
 
 			return productavailability;
